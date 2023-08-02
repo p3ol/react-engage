d5e874208c1b86647f04fcb78309e09624e93f15
Antoine Chassagne <antoine@poool.fr>
2023-08-02 09:57:51 +0200

    feat: add elements by auto create

---
+++ src/Elements/index.js
@@ -0,0 +1,75 @@
+import { useEffect, useImperativeHandle, useReducer, forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import { mockState } from '@junipero/core';
+
+import { useEngage } from '../hooks';
+
+const Elements = forwardRef(({
+  useGlobalFactory = true,
+  filters,
+  config,
+  variables,
+  texts,
+  events,
+}, ref) => {
+  const [state, dispatch] = useReducer(mockState, {
+    elements: [],
+  });
+  const {
+    lib,
+    factory:
+    globalFactory,
+    createFactory,
+    destroyFactory,
+  } = useEngage();
+
+  useImperativeHandle(ref, () => ({
+    elementsRef: state.elements,
+    destroy,
+  }));
+
+  useEffect(() => {
+    create();
+
+    return () => {
+      destroy();
+    };
+  }, [lib, globalFactory, filters, config, variables, texts, events]);
+
+  const create = async () => {
+    const factory = useGlobalFactory ? globalFactory : createFactory?.({
+      config,
+      variables,
+      texts,
+      events,
+    });
+
+    if (!factory) {
+      return;
+    }
+
+    state.elements = await factory.autoCreate(filters);
+    dispatch({ elements: state.elements });
+  };
+
+  const destroy = async () => {
+    await Promise.all(state.elements.map(element => element.destroy()));
+    destroyFactory();
+    state.elements = [];
+  };
+
+  return null;
+});
+
+Elements.displayName = 'Elements';
+
+Elements.propTypes = {
+  useGlobalFactory: PropTypes.bool,
+  filters: PropTypes.object,
+  config: PropTypes.object,
+  variables: PropTypes.object,
+  texts: PropTypes.object,
+  events: PropTypes.object,
+};
+
+export default Elements;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as EngageContext } from './EngageContext';
 export { default as Element } from './Element';
+export { default as Elements } from './Elements';
 
 export { useEngage } from './hooks';
