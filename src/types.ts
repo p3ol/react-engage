b5f1170e4f948b8a630d70dea7f5341176dbb61a
Ugo Stephant <ugo@poool.fr>
2025-08-05 17:31:59 +0200

    fix: various type issues

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 import type { Poool } from 'poool-engage';
 
 export declare type EventCallbackFunction<Props> = (props: Props) => any;
-export declare type EventCallbackObject<Props> = {
+export declare interface EventCallbackObject<Props> {
   once: true;
   callback: EventCallbackFunction<Props>;
 };
@@ -10,9 +10,30 @@ export declare type EventCallback<Props> =
   | EventCallbackFunction<Props>
   | EventCallbackObject<Props>;
 
-export declare type BaseEvents = {
-  [key in Poool.EngageEventsList]?: any
-};
+export declare type BaseEvents = Partial<Record<Poool.EngageEventsList, any>>;
+
+export declare interface EngageReadyEvent {
+  element: Record<string, any>;
+}
+
+export declare interface EngageSeenEvent {
+  element: Record<string, any>;
+}
+
+export declare interface EngageClickEvent {
+  originalEvent: MouseEvent;
+  id: string;
+  type: string;
+  url: string;
+  name: string;
+  element: Record<string, any>;
+}
+
+export declare interface EngageFormSubmitEvent {
+  fields: Record<string, any>;
+  valid: Record<string, boolean>;
+  element: Record<string, any>;
+}
 
 export declare interface EngageEvents extends BaseEvents {
   /**
@@ -20,37 +41,20 @@ export declare interface EngageEvents extends BaseEvents {
    *
    * More infos: https://www.poool.dev/docs/engage/javascript/events#ready
    */
-  ready?: EventCallback<{
-    element: {
-      [fieldKey: string]: any;
-    };
-  }>;
+  ready: EventCallback<EngageReadyEvent>;
   /**
    * Triggered when the element has been seen by the user (when it has
    * entered the browser's viewport).
    *
    * More infos: https://www.poool.dev/docs/engage/javascript/events#seen
    */
-  seen?: EventCallback<{
-    element: {
-      [fieldKey: string]: any;
-    };
-  }>;
+  seen: EventCallback<EngageSeenEvent>;
   /**
    * Triggered when a user has clicked a button/link inside the element.
    *
    * More infos: https://www.poool.dev/docs/engage/javascript/events#click
    */
-  click?: EventCallback<{
-    id: string;
-    type: string;
-    originalEvent: MouseEvent;
-    url: string;
-    name: string;
-    element: {
-      [fieldKey: string]: any;
-    };
-  }>;
+  click: EventCallback<EngageClickEvent>;
   /**
    * Triggered when a user submits a form inside an element.
    *
@@ -59,17 +63,7 @@ export declare interface EngageEvents extends BaseEvents {
    *
    * More infos: https://www.poool.dev/docs/engage/javascript/events#formSubmit
    */
-  formSubmit?: EventCallback<{
-    fields: {
-      [fieldKey: string]: any;
-    };
-    valid: {
-      [fieldKey: string]: boolean;
-    };
-    element: {
-      [fieldKey: string]: any;
-    };
-  }>;
+  formSubmit: EventCallback<EngageFormSubmitEvent>;
 }
 
 export declare interface EngageConfigCommons {
@@ -88,13 +82,13 @@ export declare interface EngageConfigCommons {
    *
    * More infos: https://www.poool.dev/docs/engage/javascript/variables
    */
-  variables?: { [key: string]: any };
+  variables?: Record<string, any>;
   /**
    * Current Engage context texts.
    *
    * More infos: https://www.poool.dev/docs/engage/javascript/texts
    */
-  texts?: { [key: string]: string };
+  texts?: Record<string, string>;
   /**
    * Current Engage context events listeners.
    *
