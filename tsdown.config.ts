0c63ebfd7e0066cc232c2b4ce9ececb538e41974
Ugo Stephant <ugo@poool.fr>
2026-08-13 17:39:44 +0200

    chore: build with tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'tsdown';
+
+import pkg from './package.json' with { type: 'json' };
+
+export default defineConfig([
+  {
+    entry: ['./src/index.ts'],
+    outDir: 'dist',
+    platform: 'neutral',
+    target: pkg.targets,
+    format: ['cjs', 'esm'],
+    deps: {
+      neverBundle: [
+        'react',
+        'react-dom',
+      ],
+    },
+    sourcemap: true,
+    dts: false,
+  },
+]);
