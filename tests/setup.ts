1b1766480568766b2af619568ff794027b376acd
Ugo Stephant <ugo@poool.fr>
2026-08-13 17:48:52 +0200

    refactor: remove useless code

--- tests/setup.ts
+++
@@ -1,5 +0,0 @@
-// jsdom is weird
-// @ts-expect-error - setImmediate is readonly
-global.setImmediate = global.setImmediate ||
-  ((fn: (...args: any[]) => void, ...args: any[]) =>
-    global.setTimeout(() => fn(...args), 0));
